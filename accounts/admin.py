a470010e194248881b7b7aedcea7db5a7b0833d2
Rehan Parveez <rehanparveez@gmail.com>
2026-04-03 10:16:43 +0500

    migration

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,8 +4,8 @@ from accounts.models import User, Profile
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-  list_display = ['email', 'phone', 'control', 'dob', 'is_admin', 'created_at', 'updated_at']
+  list_display = ['email', 'phone', 'dob', 'is_admin', 'created_at', 'updated_at']
   
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-  list_display = ['user', 'location']
+  list_display = ['user', 'control', 'location']
